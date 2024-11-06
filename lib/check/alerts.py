0f27495b787126f469c66845a6c88f16c2acb315
Jeroen van der Heijden <jeroen@cesbit.com>
2024-11-06 14:38:35 +0100

    exception

--- lib/check/alerts.py
+++ lib/check/alerts.py
@@ -7,6 +7,16 @@ async def check_alerts(
         asset: Asset,
         asset_config: dict,
         check_config: dict) -> dict:
-    token = await get_token(asset, asset_config, check_config)
-    state = await get_logs_alert(asset, check_config, token)
+    try:
+        token = await get_token(asset, asset_config, check_config)
+    except Exception:
+        logging.exception('Failed to retrieve token')
+        raise
+
+    try:
+        state = await get_logs_alert(asset, check_config, token)
+    except Exception:
+        logging.exception('Failed to get alerts')
+        raise
+
     return state
